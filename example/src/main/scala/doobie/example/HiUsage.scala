271fefb70e166e31c08de7fbaedd45e4b48c9030
Rob Norris <rob_norris@mac.com>
2014-02-10 23:25:52 -0800

    cleanup

--- example/src/main/scala/doobie/example/HiUsage.scala
+++ example/src/main/scala/doobie/example/HiUsage.scala
@@ -1,52 +1,41 @@
 package doobie.example
 
-
 import doobie._
 import doobie.hi._
 
-import scalaz._
-import Scalaz._
-import scalaz.effect._
-import scalaz.effect.IO._
+import scalaz.effect.{IO, SafeApp}
+import scalaz.effect.IO.putStrLn
 import scalaz.stream.Process
 import java.io.File
 
 // JDBC program using the high-level API
 object HiUsage extends SafeApp {
 
-  // A simple model object with a Show instance  
-  case class Country(code: String, name: String, population: Int) {
-    require(code != "CHE", "example random failure")
-  }
-  object Country {
-    implicit def show: Show[Country] = 
-      Show.showA
-  }
+  // A simple model with nested case classes
+  case class Code(code: String) { require(code != "NCL", "arbitrary failure") }
+  case class Country(code: Code, name: String, population: Int)
 
-  // treat File as a primitive column type (contrived)
-  implicit def primFile: Prim[File] =
-    Prim[String].xmap(new File(_), _.getName)
-
-  // Transactor, which handles the log and connections
-  val ta = DriverManagerTransactor[org.h2.Driver]("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
+  // Connection information
+  val ta: Transactor = 
+    DriverManagerTransactor[org.h2.Driver]("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
 
   // Entry point
   override def runc: IO[Unit] =
     examples.run(ta).except(t => putStrLn("failed: " + t))
 
-  // Some examples to run together in a single transaction
+  // Some examples to run on the same connection
   def examples: DBIO[Unit] =
     for {
       _ <- loadDatabase(new File("world.sql"))
-      _ <- countriesWithSpeakers("French").take(10).sink(putLn)
+      _ <- speakersOf("French").take(10).map(_.name).sink(putStrLn) // stream top 10 names to stdout
     } yield ()
 
+  // Load the database from a file
   def loadDatabase(f: File): DBIO[Boolean] =
-    sql"""
-      RUNSCRIPT FROM $f CHARSET 'UTF-8'
-    """.execute
+    sql"RUNSCRIPT FROM ${f.getName} CHARSET 'UTF-8'".execute
 
-  def countriesWithSpeakers(s: String): Process[DBIO, Country] =
+  // Construct a stream transformer for countries that speak the given language
+  def speakersOf(s: String): Process[DBIO, Country] =
     sql"""
       SELECT C.CODE, C.NAME, C.POPULATION
       FROM COUNTRYLANGUAGE L
@@ -54,6 +43,6 @@ object HiUsage extends SafeApp {
       ON L.COUNTRYCODE = C.CODE
       WHERE LANGUAGE = $s
       ORDER BY PERCENTAGE DESC
-      """.process[Country]
+    """.process[Country]
 
 }
