67020f26ea4453a37adebbe7633981db9aa32dbc
Rob Norris <rob_norris@mac.com>
2014-11-25 22:06:43 -0800

    unique for update

--- core/src/main/scala/doobie/util/update.scala
+++ core/src/main/scala/doobie/util/update.scala
@@ -28,6 +28,8 @@ object update {
 
     def withGeneratedKeys[K: Composite](columns: String*)(a: A): Process[ConnectionIO, K]
 
+    def withUniqueGeneratedKeys[K: Composite](columns: String*)(a: A): ConnectionIO[K]
+
     def contramap[C](f: C => A): Update[C] =
       new Update[C] {
         val sql = u.sql
@@ -36,6 +38,8 @@ object update {
         def run(c: C) = u.run(f(c))
         def withGeneratedKeys[K: Composite](columns: String*)(c: C) =
           u.withGeneratedKeys(columns: _*)(f(c))
+        def withUniqueGeneratedKeys[K: Composite](columns: String*)(c: C) =
+          u.withUniqueGeneratedKeys(columns: _*)(f(c))
       }
 
     def toUpdate0(a: A): Update0 =
@@ -46,6 +50,8 @@ object update {
         def run = u.run(a)
         def withGeneratedKeys[K: Composite](columns: String*) = 
           u.withGeneratedKeys(columns: _*)(a)
+        def withUniqueGeneratedKeys[K: Composite](columns: String*) =
+          u.withUniqueGeneratedKeys(columns: _*)(a)
       }
 
   }
@@ -60,6 +66,8 @@ object update {
         def run(a: A) = prepareStatement(sql)(set(a) >> executeUpdate)
         def withGeneratedKeys[K: Composite](columns: String*)(a: A) =
           updateWithGeneratedKeys[K](columns.toList)(sql, set(a))
+        def withUniqueGeneratedKeys[K: Composite](columns: String*)(a: A) =
+          prepareStatementS(sql0, columns.toList)(set(a) >> executeUpdateWithUniqueGeneratedKeys)
       }
 
     implicit val updateContravariant: Contravariant[Update] =
@@ -72,6 +80,7 @@ object update {
   trait Update0 extends UpdateDiagnostics {
     def run: ConnectionIO[Int]
     def withGeneratedKeys[K: Composite](columns: String*): Process[ConnectionIO, K]
+    def withUniqueGeneratedKeys[K: Composite](columns: String*): ConnectionIO[K]
   }
 
   object Update0 {
@@ -84,6 +93,8 @@ object update {
         def run = prepareStatement(sql)(executeUpdate)
         def withGeneratedKeys[K: Composite](columns: String*) =
           updateWithGeneratedKeys(columns.toList)(sql, ().point[PreparedStatementIO])
+        def withUniqueGeneratedKeys[K: Composite](columns: String*) =
+          prepareStatementS(sql0, columns.toList)(executeUpdateWithUniqueGeneratedKeys)
       }
 
   }
