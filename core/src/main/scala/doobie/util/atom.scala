59823349997776e9c6a55e4ca4d2a34206106ca7
Rob Norris <rob_norris@mac.com>
2015-02-10 23:02:20 -0800

    atom for Maybe

--- core/src/main/scala/doobie/util/atom.scala
+++ core/src/main/scala/doobie/util/atom.scala
@@ -6,7 +6,7 @@ import doobie.enum.nullability._
 import doobie.util.meta._
 import doobie.util.invariant._
 
-import scalaz.InvariantFunctor
+import scalaz.{ InvariantFunctor, Maybe }
 import scalaz.syntax.applicative._
 import scalaz.syntax.std.boolean._
 
@@ -15,12 +15,12 @@ import scala.annotation.implicitNotFound
 /** Module defining `NULL`-aware column mappings; the next step "up" from `Meta`. */
 object atom {
 
-  /** 
-   * A `Meta` together with a `Nullability` and `NULL`-aware `get/set/update`. Given an 
+  /**
+   * A `Meta` together with a `Nullability` and `NULL`-aware `get/set/update`. Given an
    * implicit `Meta[A]` you get derived `Atom[A]` and `Atom[Option[A]]`.
    */
   @implicitNotFound("Could not find or construct Atom[${A}]; ensure that ${A} has a Meta instance.")
-  sealed trait Atom[A] { outer => 
+  sealed trait Atom[A] { outer =>
     val set: (Int, A) => PreparedStatementIO[Unit]
     val update: (Int, A) => ResultSetIO[Unit]
     val get: Int => ResultSetIO[A]
@@ -37,7 +37,7 @@ object atom {
 
     def apply[A](implicit A: Atom[A]): Atom[A] = A
 
-    implicit def fromScalaType[A](implicit A: Meta[A]): Atom[A] = 
+    implicit def fromScalaType[A](implicit A: Meta[A]): Atom[A] =
       new Atom[A] {
         val get = (n: Int) => ^(A.get(n), wasNull)((a, b) => if (b) throw NonNullableColumnRead(n, A.jdbcTarget.head) else a)
         val set = (n: Int, a: A) => if (a == null) throw NonNullableParameter(n, A.jdbcTarget.head) else A.set(n, a)
@@ -45,11 +45,19 @@ object atom {
         val meta = (A, NoNulls)
       }
 
-    implicit def fromScalaTypeOption[A](implicit A: Meta[A]): Atom[Option[A]] = 
+    implicit def fromScalaTypeOption[A](implicit A: Meta[A]): Atom[Option[A]] =
       new Atom[Option[A]] {
         val get = (n: Int) => ^(A.get(n), wasNull)((a, b) => (!b) option a)
         val set = (n: Int, a: Option[A]) => a.fold(A.setNull(n))(A.set(n, _))
         val update = (n: Int, a: Option[A]) => a.fold(updateNull(n))(A.update(n, _))
+        val meta = (A, Nullable)
+      }
+
+    implicit def fromScalaTypeMaybe[A](implicit A: Meta[A]): Atom[Maybe[A]] =
+      new Atom[Maybe[A]] {
+        val get = (n: Int) => ^(A.get(n), wasNull)((a, b) => if (b) Maybe.empty[A] else Maybe.just(a))
+        val set = (n: Int, a: Maybe[A]) => a.cata(A.set(n, _), A.setNull(n))
+        val update = (n: Int, a: Maybe[A]) => a.cata(A.update(n, _), updateNull(n))
         val meta = (A, Nullable)
       }
 
@@ -60,5 +68,5 @@ object atom {
       }
 
   }
-  
+
 }
