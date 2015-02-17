7d6877426520b379b90c82a7b3e8452e86d4cb00
wedens <wedens13@yandex.ru>
2015-02-18 00:07:27 +0700

    Composite instances for HList and HNil

--- core/src/main/scala/doobie/util/composite.scala
+++ core/src/main/scala/doobie/util/composite.scala
@@ -18,13 +18,13 @@ import shapeless._
 
 import java.sql.ParameterMetaData
 
-/** 
+/**
  * Module defining a typeclass for composite database types (those that can map to multiple columns).
  */
 object composite {
 
   @implicitNotFound("Could not find or construct Composite[${A}].")
-  trait Composite[A] { c =>    
+  trait Composite[A] { c =>
     val set: (Int, A) => PS.PreparedStatementIO[Unit]
     val update: (Int, A) => RS.ResultSetIO[Unit]
     val get: Int => RS.ResultSetIO[A]
@@ -44,7 +44,7 @@ object composite {
 
     def apply[A](implicit A: Composite[A]): Composite[A] = A
 
-    implicit val compositeInvariantFunctor: InvariantFunctor[Composite] = 
+    implicit val compositeInvariantFunctor: InvariantFunctor[Composite] =
       new InvariantFunctor[Composite] {
         def xmap[A, B](ma: Composite[A], f: A => B, g: B => A): Composite[B] =
           ma.xmap(f, g)
@@ -58,35 +58,21 @@ object composite {
         val length = 1
         val meta = List(A.meta)
       }
-
   }
 
   // N.B. we're separating this out in order to make the atom ~> composite derivation higher
   // priority than the product ~> composite derivation. So this means if we have an product mapped
   // to a single column, we will get only the atomic mapping, not the multi-column one.
-  trait LowerPriorityComposite {
+  trait LowerPriorityComposite extends HListComposite {
 
     /** @group Typeclass Instances */
     implicit val productComposite: ProductTypeClass[Composite] =
       new ProductTypeClass[Composite] {
-    
-        def product[H, T <: HList](H: Composite[H], T: Composite[T]): Composite[H :: T] =
-          new Composite[H :: T] {
-            val set = (i: Int, l: H :: T) => H.set(i, l.head) >> T.set(i + H.length, l.tail)
-            val update = (i: Int, l: H :: T) => H.update(i, l.head) >> T.update(i + H.length, l.tail)
-            val get = (i: Int) => (H.get(i) |@| T.get(i + H.length))(_ :: _)
-            val length = H.length + T.length
-            val meta = H.meta ++ T.meta
-          }
 
-        def emptyProduct: Composite[HNil] =
-          new Composite[HNil] {
-            val set = (_: Int, _: HNil) => ().point[PS.PreparedStatementIO]
-            val update = (_: Int, _: HNil) => ().point[RS.ResultSetIO]
-            val get = (_: Int) => (HNil : HNil).point[RS.ResultSetIO]
-            val length = 0
-            val meta = Nil
-          }
+        def product[H, T <: HList](H: Composite[H], T: Composite[T]): Composite[H :: T] =
+          hlist[H, T](H, T)
+
+        def emptyProduct: Composite[HNil] = hnil
 
         def project[F, G](instance: => Composite[G], to: F => G, from: G => F): Composite[F] =
           new Composite[F] {
@@ -102,7 +88,25 @@ object composite {
     /** @group Typeclass Instances */
     implicit def deriveComposite[T](implicit ev: ProductTypeClass[Composite]): Composite[T] =
       macro GenericMacros.deriveProductInstance[Composite, T]
-
   }
 
+  trait HListComposite {
+    implicit def hlist[H, T <: HList](implicit H: Composite[H], T: Composite[T]): Composite[H :: T] =
+      new Composite[H :: T] {
+        val set = (i: Int, l: H :: T) => H.set(i, l.head) >> T.set(i + H.length, l.tail)
+        val update = (i: Int, l: H :: T) => H.update(i, l.head) >> T.update(i + H.length, l.tail)
+        val get = (i: Int) => (H.get(i) |@| T.get(i + H.length))(_ :: _)
+        val length = H.length + T.length
+        val meta = H.meta ++ T.meta
+      }
+
+    implicit def hnil: Composite[HNil] =
+      new Composite[HNil] {
+        val set = (_: Int, _: HNil) => ().point[PS.PreparedStatementIO]
+        val update = (_: Int, _: HNil) => ().point[RS.ResultSetIO]
+        val get = (_: Int) => (HNil : HNil).point[RS.ResultSetIO]
+        val length = 0
+        val meta = Nil
+      }
+  }
 }
