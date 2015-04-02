d9bf805e7c9e9f9ee2096679626a0cd05b6fa595
Rob Norris <rob_norris@mac.com>
2015-04-01 21:55:56 -0700

    lifted ops for copy, fastpath, lob

--- contrib/postgresql/src/main/scala/doobie/contrib/postgresql/hi/connection.scala
+++ contrib/postgresql/src/main/scala/doobie/contrib/postgresql/hi/connection.scala
@@ -2,9 +2,13 @@ package doobie.contrib.postgresql.hi
 
 import org.postgresql.{ PGConnection, PGNotification }
 
-import doobie.contrib.postgresql.free.{ pgconnection => FPGC }
+import doobie.contrib.postgresql.free.pgconnection.PGConnectionIO
+import doobie.contrib.postgresql.free.copymanager.CopyManagerIO
+import doobie.contrib.postgresql.free.fastpath.FastpathIO
+import doobie.contrib.postgresql.free.largeobjectmanager.LargeObjectManagerIO
+
 import doobie.contrib.postgresql.hi.{ pgconnection => HPGC }
-import doobie.contrib.postgresql.free.pgconnection.PGConnectionIOOps
+
 import doobie.imports._
 
 import scalaz.syntax.functor._
@@ -12,20 +16,29 @@ import scalaz.syntax.functor._
 /** Module of safe `PGConnectionIO` operations lifted into `ConnectionIO`. */
 object connection {
 
-  def liftPGConnection[A](k: FPGC.PGConnectionIO[A]): ConnectionIO[A] =
+  val pgGetBackendPID: ConnectionIO[Int] =
+    pgGetConnection(HPGC.getBackendPID)
+
+  def pgGetConnection[A](k: PGConnectionIO[A]): ConnectionIO[A] =
     FC.unwrap(classOf[PGConnection]) >>= k.transK[ConnectionIO]
 
+  def pgGetCopyAPI[A](k: CopyManagerIO[A]): ConnectionIO[A] =
+    pgGetConnection(HPGC.getCopyAPI(k))
+
+  def pgGetFastpathAPI[A](k: FastpathIO[A]): ConnectionIO[A] =
+    pgGetConnection(HPGC.getFastpathAPI(k))
+
+  def pgGetLargeObjectAPI[A](k: LargeObjectManagerIO[A]): ConnectionIO[A] =
+    pgGetConnection(HPGC.getLargeObjectAPI(k))
+
   val pgGetNotifications: ConnectionIO[List[PGNotification]] =
-    liftPGConnection(HPGC.getNotifications)
+    pgGetConnection(HPGC.getNotifications)
 
   val pgGetPrepareThreshold: ConnectionIO[Int] =
-    liftPGConnection(HPGC.getPrepareThreshold)
+    pgGetConnection(HPGC.getPrepareThreshold)
 
   def pgSetPrepareThreshold(threshold: Int): ConnectionIO[Unit] =
-    liftPGConnection(HPGC.setPrepareThreshold(threshold))
-
-  private def execVoid(sql: String): ConnectionIO[Unit] =
-    HC.prepareStatement(sql)(HPS.executeUpdate).void
+    pgGetConnection(HPGC.setPrepareThreshold(threshold))
 
   /** Construct a program that starts listening on the given channel. */
   def pgListen(channel: String): ConnectionIO[Unit] = 
@@ -34,5 +47,10 @@ object connection {
   /** Construct a program that stops listening on the given channel. */
   def pgUnlisten(channel: String): ConnectionIO[Unit] = 
     execVoid("UNLISTEN " + channel)
+
+
+  // a helper
+  private def execVoid(sql: String): ConnectionIO[Unit] =
+    HC.prepareStatement(sql)(HPS.executeUpdate).void
 
 }
