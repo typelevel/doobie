3fa504442ba581e05c0a2076638fb1bca6646e06
Rob Norris <rob_norris@mac.com>
2015-04-03 14:48:29 -0700

    added pgNotify, improved doc

--- contrib/postgresql/src/main/scala/doobie/contrib/postgresql/hi/connection.scala
+++ contrib/postgresql/src/main/scala/doobie/contrib/postgresql/hi/connection.scala
@@ -40,11 +40,35 @@ object connection {
   def pgSetPrepareThreshold(threshold: Int): ConnectionIO[Unit] =
     pgGetConnection(HPGC.setPrepareThreshold(threshold))
 
-  /** Construct a program that starts listening on the given channel. */
+  /** 
+   * Construct a program that notifies on the given channel. Note that the channel is NOT sanitized;
+   * it cannot be passed as a parameter and is simply interpolated into the statement. DO NOT pass 
+   * user input here.
+   */
+  def pgNotify(channel: String): ConnectionIO[Unit] = 
+    execVoid("LISTEN " + channel)
+
+  /** 
+   * Construct a program that notifies on the given channel, with a payload. Note that neither the 
+   * channel nor the payload are sanitized; neither can be passed as parameters and are simply 
+   * interpolated into the statement. DO NOT pass user input here.
+   */
+  def pgNotify(channel: String, payload: String): ConnectionIO[Unit] = 
+    execVoid(s"LISTEN $channel, '$payload'")
+
+  /** 
+   * Construct a program that starts listening on the given channel. Note that the channel is NOT 
+   * sanitized; it cannot be passed as a parameter and is simply interpolated into the statement. 
+   * DO NOT pass user input here.
+   */
   def pgListen(channel: String): ConnectionIO[Unit] = 
     execVoid("LISTEN " + channel)
 
-  /** Construct a program that stops listening on the given channel. */
+  /** 
+   * Construct a program that stops listening on the given channel. Note that the channel is NOT 
+   * sanitized; it cannot be passed as a parameter and is simply interpolated into the statement. 
+   * DO NOT pass user input here.
+   */
   def pgUnlisten(channel: String): ConnectionIO[Unit] = 
     execVoid("UNLISTEN " + channel)
 
