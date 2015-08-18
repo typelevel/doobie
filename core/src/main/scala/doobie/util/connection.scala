38d15e351e8b6881f55dba95a8dece7df1e90a64
Rob Norris <rob_norris@mac.com>
2015-08-17 21:51:33 -0700

    move syntax

--- core/src/main/scala/doobie/util/connection.scala
+++ core/src/main/scala/doobie/util/connection.scala
@@ -7,8 +7,10 @@ import scalaz.{ Monad, Catchable }
 
 import java.sql.Connection
 
+/** Typeclass instances for `java.sql.Connection`. */
 object connection {
 
+  /** @group Typeclass Instances */
   implicit def connectionConnector[M[_]: Monad: Capture: Catchable]: Connector[M, Connection] =
     Connector.instance(Monad[M].point(_))
 
