c5c90b78b6a5fd0151806c5a1f9bab36e5a8b8dc
Jisoo Park <xxxyel@gmail.com>
2016-10-09 16:41:21 +0900

    Run tut in Travis builds

--- project/travis-build.sh
+++ project/travis-build.sh
@@ -14,9 +14,7 @@ fi
 
 if [ "$TRAVIS_SCALA_VERSION" = "$MAIN_SCALA_VERSION" ]; then
     echo "Testing with docs for Scala $MAIN_SCALA_VERSION"
-    exec sbt ++$MAIN_SCALA_VERSION scalastyle compile test:compile test && \
-      exec sbt ++$MAIN_SCALA_VERSION docs/tut && \
-      exec sbt ++$MAIN_SCALA_VERSION docs_cats/tut
+    exec sbt ++$MAIN_SCALA_VERSION scalastyle compile test:compile test docs/tut docs_cats/tut
 else
     echo "Testing without docs for Scala $TRAVIS_SCALA_VERSION"
     exec sbt ++$TRAVIS_SCALA_VERSION core/test h2/test hikari/test postgres/test specs2/test
