d10a81fb93d553ef6dc4812cc529eefe45e00bcd
Jisoo Park <xxxyel@gmail.com>
2016-11-03 20:05:16 +0900

    Disable cats build in Scala 2.10.x

--- project/travis-build.sh
+++ project/travis-build.sh
@@ -1,20 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 cd `dirname $0`/..
 
-if [ -z "$MAIN_SCALA_VERSION" ]; then
+if [[ -z "$MAIN_SCALA_VERSION" ]]; then
     >&2 echo "Environment variable MAIN_SCALA_VERSION is not set. Check .travis.yml."
     exit 1
 fi
 
-if [ -z "$TRAVIS_SCALA_VERSION" ]; then
+if [[ -z "$TRAVIS_SCALA_VERSION" ]]; then
     echo "Environment variable TRAVIS_SCALA_VERSION is not set. Using MAIN_SCALA_VERSION: $MAIN_SCALA_VERSION"
     TRAVIS_SCALA_VERSION=$MAIN_SCALA_VERSION
 fi
 
-if [ "$TRAVIS_SCALA_VERSION" = "$MAIN_SCALA_VERSION" ]; then
+if [[ "$TRAVIS_SCALA_VERSION" = "$MAIN_SCALA_VERSION" ]]; then
     echo "Testing with docs for Scala $MAIN_SCALA_VERSION"
     exec sbt ++$MAIN_SCALA_VERSION scalastyle compile test:compile test docs/tut docs_cats/tut
+elif [[ "$TRAVIS_SCALA_VERSION" == 2.10.* ]]; then
+    echo "Testing scalaz dialect without docs for Scala $MAIN_SCALA_VERSION"
+    exec sbt ++$MAIN_SCALA_VERSION core/test h2/test hikari/test postgres/test specs2/test scalatest/test
 else
     echo "Testing without docs for Scala $TRAVIS_SCALA_VERSION"
     exec sbt ++$TRAVIS_SCALA_VERSION compile test:compile test
