98a1617ebc207702ea2bc1dd51679743cfd25075
btre <benjamin.trenker@gmail.com>
2017-01-21 13:46:32 +0100

    added imports

--- yax/refined/src/main/scala/doobie/refined/package.scala
+++ yax/refined/src/main/scala/doobie/refined/package.scala
@@ -2,7 +2,12 @@ package doobie
 
 package object refined {
 
+  import eu.timepit.refined.api.{Refined, Validate}
+  import eu.timepit.refined.auto._
+  import eu.timepit.refined._
   import scala.reflect.runtime.universe.TypeTag
+
+  import doobie.imports._
 
   implicit def refinedMeta[T, P](implicit metaT: Meta[T], v: Validate[T, P], tag: TypeTag[T Refined P]): Meta[T Refined P] =
     metaT.xmap[T Refined P](
