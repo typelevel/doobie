4f5e81a60b49af0279847b4b0f2aa23f605ecc90
Colt Frederickson <coltfred@gmail.com>
2017-02-20 12:20:05 -0700

    Added set and setOpt combinators

--- yax/core/src/main/scala/doobie/util/fragments.scala
+++ yax/core/src/main/scala/doobie/util/fragments.scala
@@ -52,4 +52,12 @@ object fragments {
   def whereOrOpt(fs: Option[Fragment]*): Fragment =
     whereOr(fs.flatten: _*)
 
+  /** Returns `SET f1, f2, ... fn` or the empty fragment if `fs` is empty. */
+  def set(fs: Fragment*): Fragment = 
+    if (fs.isEmpty) Fragment.empty else fr"SET" ++ fs.toList.intercalate(fr",")
+
+  /** Returns `SET f1, f2, ... fn` for defined `f`, if any, otherwise the empty fragment. */
+  def setOpt(fs: Option[Fragment]*): Fragment = 
+    set(fs.flatten: _*)
+
 }
