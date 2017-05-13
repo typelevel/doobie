74d4f660444ef23d7e473d687c40c0e257ec86b6
Rob Norris <rob_norris@mac.com>
2017-05-13 07:46:20 -0700

    typo

--- yax/scalatest/src/main/scala/doobie/scalatest/imports.scala
+++ yax/scalatest/src/main/scala/doobie/scalatest/imports.scala
@@ -5,7 +5,7 @@ object imports {
   type Checker[M[_]] = doobie.scalatest.Checker[M]
   type IOLiteChecker = doobie.scalatest.IOLiteChecker
 
-  @deprecated("Use CheckerIOLite.", "0.4.2")
+  @deprecated("Use IOLiteChecker.", "0.4.2")
   type QueryChecker = IOLiteChecker
 
 }
