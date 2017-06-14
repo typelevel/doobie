601d1b056a7602f76f151d4a8bbe825ab0068ccd
Rob Norris <rob_norris@mac.com>
2017-06-14 13:53:34 -0500

    initial

---
+++ yax/postgres/src/main/scala/doobie/postgres/csv.scala
@@ -0,0 +1,157 @@
+package doobie.postgres
+
+import doobie.imports.{ ConnectionIO, Fragment }
+import doobie.postgres.imports.{ PHC, PFCM }
+
+#+scalaz
+import scalaz.Foldable
+import scalaz.syntax.foldable._
+import scalaz.std.string._
+#-scalaz
+#+cats
+import cats.Foldable
+import cats.syntax.foldable._
+import cats.instances.string._
+#-cats
+
+import shapeless.{ HList, HNil, ::, <:!<, Generic }
+
+object csv {
+
+  trait Csv[A] { outer =>
+    def unsafeEncode(a: A, quote: Char, esc: Char): StringBuilder => StringBuilder
+    final def encode(a: A, quote: Char, esc: Char): String =
+      unsafeEncode(a, quote, esc)(new StringBuilder).toString
+    final def contramap[B](f: B => A): Csv[B] =
+      Csv.instance((b, q, e) => outer.unsafeEncode(f(b), q, e))
+  }
+  object Csv extends CsvInstances {
+    def apply[A](implicit ev: Csv[A]): ev.type = ev
+    def instance[A](f: (A, Char, Char) => StringBuilder => StringBuilder): Csv[A] =
+      new Csv[A] {
+        def unsafeEncode(a: A, quote: Char, esc: Char) = sb => f(a, quote, esc)(sb)
+      }
+  }
+
+  trait CsvInstances extends CsvInstances0 { this: Csv.type =>
+
+    // String requires escaping
+    implicit val stringInstance: Csv[String] =
+      instance { (s, q, e) => sb =>
+        sb.append(q)
+        s.foreach { c =>
+          if (c == q) sb.append(e)
+          sb.append(c)
+        }
+        sb.append(q)
+      }
+
+    // Primitive Numerics
+    implicit val intInstance:    Csv[Int]    = instance { (n, _, _) => _ append n }
+    implicit val shortInstance:  Csv[Short]  = instance { (n, _, _) => _ append n }
+    implicit val longInstance:   Csv[Long]   = instance { (n, _, _) => _ append n }
+    implicit val floatInstance:  Csv[Float]  = instance { (n, _, _) => _ append n }
+    implicit val doubleInstance: Csv[Double] = instance { (n, _, _) => _ append n }
+
+    // Big Numerics
+    implicit val bigDecimalInstance: Csv[BigDecimal] = instance { (n, _, _) => _ append n.toString }
+    implicit val bigIntInstance:     Csv[BigInt]     = instance { (n, _, _) => _ append n.toString }
+
+    // Boolean
+    implicit val booleanInstance: Csv[Boolean] =
+      instance {
+        case (true,  q, e) => _ append "TRUE"
+        case (false, q, e) => _ append "FALSE"
+      }
+
+    // Date, Time, etc.
+
+    // Byte arrays in E'\xx01A3DD...' format.
+    implicit val byteArrayInstance: Csv[Array[Byte]] =
+      instance { (bs, q, e) => sb =>
+        sb.append("""E'\\x""")
+        sb.append(BigInt(1, bs).toString(16))
+        sb.append('\'')
+      }
+
+    // Any non-option Csv can be lifted to Option
+    implicit def option[A](
+      implicit csv: Csv[A],
+               nope: A <:!< Option[X] forSome { type X }
+    ): Csv[Option[A]] =
+      instance {
+        case (Some(a), q, e) => csv.unsafeEncode(a, q, e)
+        case (None, q, e)    => identity
+      }
+
+    // HNil isn't a valid Csv but a single-element HList is
+    implicit def single[A](
+      implicit csv: Csv[A]
+    ): Csv[A :: HNil] =
+      csv.contramap(_.head)
+
+    // HLists of more that one element
+    implicit def multiple[H, T <: HList](
+      implicit h: Csv[H],
+               t: Csv[T]
+    ): Csv[H :: T] =
+      instance { (l, q, e) => sb =>
+        h.unsafeEncode(l.head, q, e)(sb)
+        sb.append(',')
+        t.unsafeEncode(l.tail, q, e)(sb)
+      }
+
+    // Generic
+    implicit def generic[A, B](
+      implicit gen: Generic.Aux[A, B],
+               csv: Csv[B]
+    ): Csv[A] =
+      csv.contramap(gen.to)
+
+  }
+
+  trait CsvInstances0 extends CsvInstances1 { this: Csv.type =>
+
+    // Foldable
+    implicit def iterableInstance[F[_], A](
+      implicit ev: Csv[A],
+               f:  F[A] => Iterable[A]
+    ): Csv[F[A]] =
+      instance { (fa, q, e) => sb =>
+        var first = true
+        sb.append("ARRAY[")
+        f(fa).foreach { a =>
+          if (first) first = false
+          else sb.append(',')
+          ev.unsafeEncode(a, q, e)(sb)
+        }
+        sb.append(']')
+        sb
+      }
+
+  }
+
+  trait CsvInstances1 { this: Csv.type =>
+
+    // Foldable, not as fast
+    implicit def foldableInstance[F[_]: Foldable, A](
+      implicit ev: Csv[A]
+    ): Csv[F[A]] =
+      iterableInstance[List, A].contramap(_.toList)
+
+  }
+
+
+  implicit class CsvOps[A](a: A)(implicit ev: Csv[A]) {
+    def csv = ev.encode(a, '"', '"')
+  }
+
+  implicit class PgFragmentOps(f: Fragment) {
+    import java.io._
+    def copyIn[F[_]: Foldable, A: Csv](fa: F[A]): ConnectionIO[Long] = {
+      val data: Reader = new StringReader(fa.foldMap(s => s.csv + "\n"))
+      PHC.pgGetCopyAPI(PFCM.copyIn(f.query.sql, data))
+    }
+  }
+
+}
