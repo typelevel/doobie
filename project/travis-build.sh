7dc63d32b6dec1bef85d306aa5986bf6abc098b5
Rob Norris <rob_norris@mac.com>
2017-08-06 20:32:26 -0700

    travis

--- project/travis-build.sh
+++ project/travis-build.sh
@@ -14,12 +14,7 @@ fi
 
 if [[ "$TRAVIS_SCALA_VERSION" = "$MAIN_SCALA_VERSION" ]]; then
     echo "Testing with docs for Scala $MAIN_SCALA_VERSION"
-    exec sbt ++$MAIN_SCALA_VERSION compile test:compile test
-    exec sbt ++$MAIN_SCALA_VERSION docs/tutQuick
-    exec sbt ++$MAIN_SCALA_VERSION docs_cats/tutQuick
-elif [[ "$TRAVIS_SCALA_VERSION" == 2.10.* ]]; then
-    echo "Testing scalaz dialect without docs for Scala $MAIN_SCALA_VERSION"
-    exec sbt ++$MAIN_SCALA_VERSION core/test h2/test hikari/test postgres/test specs2/test scalatest/test
+    exec sbt ++$MAIN_SCALA_VERSION compile test:compile test docs/tutQuick
 else
     echo "Testing without docs for Scala $TRAVIS_SCALA_VERSION"
     exec sbt ++$TRAVIS_SCALA_VERSION compile test:compile test
