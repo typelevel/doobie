b2e1b15bf0163e83de75e43fc95ea3a78db3036e
Rob Norris <rob_norris@mac.com>
2017-11-09 19:50:16 -0800

    optimize

--- modules/postgres/src/main/scala/doobie/postgres/syntax/FragmentSyntax.scala
+++ modules/postgres/src/main/scala/doobie/postgres/syntax/FragmentSyntax.scala
@@ -2,15 +2,15 @@ package doobie
 package postgres
 package syntax
 
-import cats.Foldable
+import cats.{ Foldable }
 import cats.syntax.foldable._
-import cats.instances.string._
-import java.io.{ Reader, StringReader }
+import java.io.StringReader
 
 class FragmentOps(f: Fragment) {
   def copyIn[F[_]: Foldable, A](fa: F[A])(implicit ev: Csv[A]): ConnectionIO[Long] = {
-    val data: Reader = new StringReader(fa.foldMap(s => ev.encode(s, '"', '"') + "\n")) // this is N^2 string append, oops
-    PHC.pgGetCopyAPI(PFCM.copyIn(f.query.sql, data))
+    // Fold with a StringBuilder and unsafeEncode to minimize allocations
+    val data = fa.foldLeft(new StringBuilder)((b, a) => ev.unsafeEncode(a, '"', '"')(b).append("\n")).toString
+    PHC.pgGetCopyAPI(PFCM.copyIn(f.query.sql, new StringReader(data)))
   }
 }
 
