831fc0c8bb8a246722f8e6b93f22a73c6c353a96
Rob Norris <rob_norris@mac.com>
2017-11-19 10:19:12 -0800

    stream ctor for h2 transactor

--- modules/h2/src/main/scala/doobie/h2/H2Transactor.scala
+++ modules/h2/src/main/scala/doobie/h2/H2Transactor.scala
@@ -6,15 +6,22 @@ package doobie
 package h2
 
 import cats.effect.Async
+import fs2.Stream
 import org.h2.jdbcx.JdbcConnectionPool
 
 object H2Transactor {
 
+  /** Constructs a program that constructs a new H2Transactor. */
   @deprecated("This method has been renamed `newH2Transactor` to help clarify usage", "doobie 0.5.0")
   def apply[M[_]: Async](url: String, user: String, pass: String): M[H2Transactor[M]] =
     newH2Transactor(url, user, pass)
 
+  /** Constructs a program that constructs a new H2Transactor. */
   def newH2Transactor[M[_]: Async](url: String, user: String, pass: String): M[H2Transactor[M]] =
     Async[M].delay(Transactor.fromDataSource[M](JdbcConnectionPool.create(url, user, pass)))
+
+  /** Constructs a stream that emits a single `HikariTransactor` with guaranteed cleanup. */
+  def stream[M[_]: Async](url: String, user: String, pass: String) : Stream[M, H2Transactor[M]] =
+    Stream.bracket(newH2Transactor(url, user, pass))(Stream.emit(_), _.configure(ds => Async[M].delay(ds.dispose())))
 
 }
