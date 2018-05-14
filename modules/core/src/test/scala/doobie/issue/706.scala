bdc0a5e23f20c06588b5ab9b45fbc51ed02afdda
Rob Norris <rob_norris@mac.com>
2018-05-14 14:35:14 -0700

    added sanity test

---
+++ modules/core/src/test/scala/doobie/issue/706.scala
@@ -0,0 +1,43 @@
+// Copyright (c) 2013-2018 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package doobie.issue
+
+import cats._
+import cats.implicits._
+import cats.effect.{ Async, IO }
+import doobie._, doobie.implicits._
+import org.scalacheck.Prop.forAll
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+import scala.Predef._
+
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+object `706` extends Specification with ScalaCheck {
+
+  val xa = Transactor.fromDriverManager[IO](
+    "org.h2.Driver",
+    "jdbc:h2:mem:issue-706;DB_CLOSE_DELAY=-1",
+    "sa", ""
+  )
+
+  val setup: ConnectionIO[Unit] =
+    sql"CREATE TABLE IF NOT EXISTS test (value INTEGER)".update.run.void
+
+  def insert[F[_]: Foldable, A: Composite](as: F[A]): ConnectionIO[Int] =
+    Update[A]("INSERT INTO test VALUES (?)").updateMany(as)
+
+  "updateMany" should {
+
+    "work correctly for valid inputs" ! forAll { (ns: List[Int]) =>
+      val prog = setup *> insert(ns)
+      prog.transact(xa).unsafeRunSync must_== ns.length
+    }
+
+    // TODO: add a case for invalid inputs if we can find one that doesn't cause an
+    // exception to be thrown.
+
+  }
+
+}
