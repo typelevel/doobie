17f5a823e378fb3f835ac30ef6632b6b3be337bd
Rob Norris <rob_norris@mac.com>
2018-05-16 16:32:52 -0700

    simple orm

---
+++ modules/example/src/main/scala/example/Orm.scala
@@ -0,0 +1,127 @@
+// Copyright (c) 2013-2018 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package example
+
+import cats.effect.IO
+import cats.implicits._
+import doobie._, doobie.implicits._
+import fs2.Stream
+import shapeless._
+import shapeless.ops.record._
+import shapeless.ops.hlist._
+
+/**
+ * A super-simple ORM for super-simple data types. We assume auto-generated keys, represented
+ * externally, and columns map 1:1 with fields and have the same names.
+ */
+object Orm {
+
+  // to silence unused warnings
+  def void[A](a: A): Unit = (a, ())._2
+
+  trait Dao[A] {
+    type Key
+    def insert(a: A): ConnectionIO[Key]
+    def find(k: Key): ConnectionIO[Option[A]]
+    def findAll: Stream[ConnectionIO, A]
+    def update(k: Key, a: A): ConnectionIO[Int]
+    def delete(k: Key): ConnectionIO[Int]
+  }
+  object Dao {
+
+    type Aux[A, K] = Dao[A] { type Key = K }
+
+    def apply[A](implicit ev: Dao[A]): Aux[A, ev.Key] = ev
+
+    object derive {
+      def apply[A, K] = new Partial[A, K]
+      class Partial[A, K] {
+        def apply[R <: HList, S <: HList](table: String, keyCol: String)(
+          implicit ev: LabelledGeneric.Aux[A, R],
+                   co: Composite[A],
+                   ks: Keys.Aux[R, S],
+                   tl: ToList[S, Symbol],
+                   ci: Composite[K]
+        ): Aux[A, K] =
+          new Dao[A] {
+            void(ev)
+
+            type Key = K
+            val cols = ks.apply.toList.map(_.name)
+
+            def insert(a: A): ConnectionIO[Key] =
+              Update[A](s"""
+                INSERT INTO $table (${cols.mkString(", ")})
+                VALUES (${cols.as("?").mkString(", ")})
+              """).withUniqueGeneratedKeys[Key](keyCol)(a)
+
+            def find(key: Key): ConnectionIO[Option[A]] =
+              Query[Key, A](s"""
+                SELECT ${cols.mkString(", ")}
+                FROM $table
+                WHERE $keyCol = ?
+              """).option(key)
+
+           def findAll: Stream[ConnectionIO, A] =
+              Query0[A](s"""
+                SELECT ${cols.mkString(", ")}
+                FROM $table
+              """).stream
+
+            def update(k: Key, a: A): ConnectionIO[Int] =
+              Update[(A, Key)](s"""
+                UPDATE $table
+                SET ${cols.map(_ + " = ?").mkString(", ")}
+                WHERE $keyCol = ?
+              """).run((a, k))
+
+            def delete(k: Key): ConnectionIO[Int] = {
+              Update[Key](s"""
+                DELETE FROM $table
+                WHERE $keyCol = ?
+              """).run(k)
+            }
+
+          }
+      }
+    }
+
+  }
+
+  // a little usage example
+
+  final case class Neighbor(name: String, age: Int)
+  object Neighbor {
+    implicit val dao: Dao.Aux[Neighbor, Int] =
+      Dao.derive[Neighbor, Int]("neighbor", "id")
+  }
+
+  val ddl: ConnectionIO[Unit] =
+    sql"""
+      CREATE TABLE IF NOT EXISTS neighbor (
+        id   SERIAL,
+        name VARCHAR NOT NULL,
+        age  INT     NOT NULL
+      )
+    """.update.run.void
+
+  val prog: ConnectionIO[String] = {
+    val dn = Dao[Neighbor]
+    import dn._
+    for {
+      _  <- ddl
+      ka <- insert(Neighbor("Alice", 42))
+      kb <- insert(Neighbor("Bob", 42))
+      oa <- find(ka)
+      _  <- delete(kb)
+    } yield s"Did some stuff. Keys were $ka and $kb. Selected value was $oa."
+  }
+
+  val xa = Transactor.fromDriverManager[IO]("org.postgresql.Driver", "jdbc:postgresql:world", "postgres", "")
+
+  def main(args: Array[String]): Unit =
+    println(prog.transact(xa).unsafeRunSync)
+
+}
