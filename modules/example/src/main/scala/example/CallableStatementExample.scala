3370947f30b69f4bb508bf2d3e8c1d0ae93d99ef
Arber Shabhasa <ashabhasa@gmail.com>
2018-05-20 22:45:48 +0200

    Add a callable statement example.

---
+++ modules/example/src/main/scala/example/CallableStatementExample.scala
@@ -0,0 +1,42 @@
+// Copyright (c) 2013-2018 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package example
+
+import cats.effect.IO
+import doobie._
+import doobie.enum.JdbcType.Other
+import doobie.free.connection.ConnectionIO
+import doobie.implicits._
+import doobie.util.transactor.Transactor
+import cats.implicits._
+
+
+object CallableStatementExample {
+
+  def names(limit: Int): ConnectionIO[List[String]] =
+    HC.prepareCall("{ call getCountries(?, ?) }") {
+      for {
+        _  <- FCS.setInt(1, limit)
+        _  <- FCS.registerOutParameter(2, Other.toInt)
+        _  <- FCS.execute
+        rs <- FCS.getObject(2).map(_.asInstanceOf[java.sql.ResultSet])
+        ns <- FCS.embed(rs, HRS.list[String] guarantee FRS.close)
+      } yield ns
+    }
+
+  val xa: Transactor[IO] =
+    Transactor.fromDriverManager[IO]("org.postgresql.Driver", "jdbc:postgresql:world", "postgres", "")
+
+  def runc: IO[Unit] =
+    for {
+      ns <- names(10).transact(xa)
+      _  <- ns.traverse(s => IO(println(s)))
+    } yield ()
+
+
+  def main(args: Array[String]): Unit = {
+    runc.unsafeRunSync()
+  }
+}
