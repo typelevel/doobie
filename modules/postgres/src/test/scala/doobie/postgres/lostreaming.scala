6edcff103a52718d9788ec6ccaaa949b7a1a9f78
Rob Norris <rob_norris@mac.com>
2018-09-10 10:20:37 -0700

    remove unused import

--- modules/postgres/src/test/scala/doobie/postgres/lostreaming.scala
+++ modules/postgres/src/test/scala/doobie/postgres/lostreaming.scala
@@ -14,8 +14,6 @@ import doobie._, doobie.implicits._
 import doobie.postgres._, doobie.postgres.implicits._
 import org.specs2.mutable.Specification
 import org.specs2.ScalaCheck
-
-import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
 
 @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
