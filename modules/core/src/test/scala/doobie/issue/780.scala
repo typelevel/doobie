e40e0849f1d15d68050f0455493d480a075ea97f
Jacob Barber <jacob.barber@bamtechmedia.com>
2018-11-12 11:54:04 -0500

    Fix the stuff, fix a couple typos

---
+++ modules/core/src/test/scala/doobie/issue/780.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2013-2018 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package doobie.issue
+
+import doobie._, doobie.implicits._
+import org.specs2.mutable.Specification
+import scala.Predef._
+import shapeless.{::, HNil}
+
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+object `780` extends Specification {
+
+  "deriving instances" should {
+    "work correctly for Param from class scope" in {
+      class Foo[A: Param, B: Param] {
+        Param[A :: B :: HNil]
+      }
+      true
+    }
+
+    "work correctly for Write from class scope" in {
+      class Foo[A: Write, B: Write] {
+        Write[A :: B :: HNil]
+      }
+      true
+    }
+  }
+
+}
