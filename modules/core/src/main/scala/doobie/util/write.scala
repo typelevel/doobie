37fe3dec2a3583c7129d6b85707e199a32b98f60
Rob Norris <rob_norris@mac.com>
2019-10-03 14:51:40 -0500

    tess

--- modules/core/src/main/scala/doobie/util/write.scala
+++ modules/core/src/main/scala/doobie/util/write.scala
@@ -11,7 +11,6 @@ import java.sql.{ PreparedStatement, ResultSet }
 import shapeless.{ HList, HNil, ::, Generic, Lazy, <:!< }
 import shapeless.labelled.{ FieldType }
 import doobie.util.fragment.Fragment
-import doobie.util.pos.Pos
 import doobie.util.param.Param.Elem
 
 final class Write[A](
