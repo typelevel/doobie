45ce73d169c9b9253df6c532f7e448bb87e63ab8
k.shelopugin <k.shelopugin@tinkoff.ru>
2019-10-25 12:54:00 +0300

    Implement Postgres COPY stream support

--- modules/postgres/src/main/scala/doobie/postgres/syntax/FragmentSyntax.scala
+++ modules/postgres/src/main/scala/doobie/postgres/syntax/FragmentSyntax.scala
@@ -6,9 +6,14 @@ package doobie.postgres.syntax
 
 import cats.Foldable
 import cats.implicits._
+import cats.effect.syntax.bracket._
+import cats.effect.{ConcurrentEffect, LiftIO}
 import doobie._
 import doobie.implicits._
 import doobie.postgres._
+import fs2._
+import fs2.io._
+import fs2.text._
 import java.io.StringReader
 
 class FragmentOps(f: Fragment) {
@@ -23,14 +28,39 @@ class FragmentOps(f: Fragment) {
     // rows is an error so we shortcut on empty input.
     // TODO: stream this rather than constructing the string in memory.
     if (fa.isEmpty) 0L.pure[ConnectionIO] else {
-      val data = fa.foldLeft(new StringBuilder) { (b, a) =>
-        ev.unsafeEncode(a, b)
-        b.append("\n")
-      } .toString
+      val data = foldToString(fa)
       PHC.pgGetCopyAPI(PFCM.copyIn(f.query.sql, new StringReader(data)))
     }
   }
 
+  /**
+   * Given a fragment of the form `COPY table (col, ...) FROM STDIN` construct a
+   * `ConnectionIO` that inserts the values provided by `stream`, returning the number of affected
+   * rows. Chunks input `stream` for more efficient sending to `STDIN` with `minChunkSize`.
+   */
+  def copyIn[F[_], A](stream: Stream[F, A], minChunkSize: Int)(
+    implicit
+    F: ConcurrentEffect[F],
+    ev: Text[A]): ConnectionIO[Long] = {
+
+    val byteStream: Stream[F, Byte] =
+      stream.chunkMin(minChunkSize).map(chunk => foldToString(chunk)).through(utf8Encode)
+
+    liftToCIO(toInputStreamResource(byteStream).allocated).flatMap {
+      case (stream, cleanup) =>
+        PHC.pgGetCopyAPI(PFCM.copyIn(f.query.sql, stream)).guarantee(liftToCIO(cleanup))
+    }
+  }
+
+  private def liftToCIO[F[_], A](ioa: F[A])(implicit F: ConcurrentEffect[F], L: LiftIO[ConnectionIO]): ConnectionIO[A] =
+    L.liftIO(F.toIO(ioa))
+
+  /** Folds given `F` to string, encoding each `A` with `Text` instance and joining resulting strings with `\n` */
+  private def foldToString[F[_]: Foldable, A](fa: F[A])(implicit ev: Text[A]): String =
+    fa.foldLeft(new StringBuilder) { (b, a) =>
+      ev.unsafeEncode(a, b)
+      b.append("\n")
+    }.toString
 }
 
 trait ToFragmentOps {
