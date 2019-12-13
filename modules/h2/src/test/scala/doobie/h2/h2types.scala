8a659293baded4bf2980db34e47c819866f68eae
Andras Zsamboki <zsambeek.andras@gmail.com>
2019-12-13 21:27:42 +0100

    Add tests for JSR-310 in h2types

--- modules/h2/src/test/scala/doobie/h2/h2types.scala
+++ modules/h2/src/test/scala/doobie/h2/h2types.scala
@@ -4,11 +4,16 @@
 
 package doobie.h2
 
-import cats.effect.{ ContextShift, IO }
-import doobie._, doobie.implicits._
+import java.time.{Instant, LocalDateTime, ZoneId, ZoneOffset}
+
+import cats.effect.{ContextShift, IO}
+import doobie._
+import doobie.implicits._
 import doobie.h2.implicits._
 import java.util.UUID
+
 import org.specs2.mutable.Specification
+
 import scala.concurrent.ExecutionContext
 import com.github.ghik.silencer.silent
 
@@ -52,6 +57,20 @@ class h2typesspec extends Specification {
       }
     }
 
+  @SuppressWarnings(Array("org.wartremover.warts.StringPlusAny"))
+  def testInOutWithCustomMatch[A, B](col: String, a: A, f: A=>B)(implicit m: Get[A], p: Put[A]) =
+    s"Mapping for $col as ${m.typeStack}" >> {
+      s"write+read $col as ${m.typeStack}" in {
+        inOut(col, a).transact(xa).attempt.unsafeRunSync.map(f) must_== Right(a).map(f)
+      }
+      s"write+read $col as Option[${m.typeStack}] (Some)" in {
+        inOutOpt[A](col, Some(a)).transact(xa).attempt.unsafeRunSync.map(_.map(f)) must_== Right(Some(a)).map(_.map(f))
+      }
+      s"write+read $col as Option[${m.typeStack}] (None)" in {
+        inOutOpt[A](col, None).transact(xa).attempt.unsafeRunSync must_== Right(None)
+      }
+    }
+
   @SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
   def skip(col: String, msg: String = "not yet implemented") =
     s"Mapping for $col" >> {
@@ -60,23 +79,34 @@ class h2typesspec extends Specification {
 
   testInOut[Int]("INT", 123)
   testInOut[Boolean]("BOOLEAN", true)
-  testInOut[Byte]("TINYINT",  123)
+  testInOut[Byte]("TINYINT", 123)
   testInOut[Short]("SMALLINT", 123)
   testInOut[Long]("BIGINT", 123)
   testInOut[BigDecimal]("DECIMAL", 123.45)
-  testInOut[java.sql.Time]("TIME", new java.sql.Time(3,4,5)) : @silent
-  testInOut[java.sql.Date]("DATE", new java.sql.Date(4,5,6)) : @silent
-  testInOut[java.time.LocalDate]("DATE", java.time.LocalDate.of(4,5,6))
+  testInOut[java.sql.Time]("TIME", new java.sql.Time(3, 4, 5)): @silent
+  testInOut[java.sql.Date]("DATE", new java.sql.Date(4, 5, 6)): @silent
+  testInOut[java.time.LocalDate]("DATE", java.time.LocalDate.of(4, 5, 6))
   testInOut[java.sql.Timestamp]("TIMESTAMP", new java.sql.Timestamp(System.currentTimeMillis))
   testInOut[java.time.Instant]("TIMESTAMP", java.time.Instant.now)
-  testInOut[List[Byte]]("BINARY", BigInt("DEADBEEF",16).toByteArray.toList)
+  testInOut[java.time.LocalTime]("TIME", java.time.LocalTime.of(2, 3))
+  testInOut[java.time.LocalDateTime]("TIMESTAMP", java.time.LocalDateTime.of(1, 2, 3, 4, 5))
+  testInOutWithCustomMatch[java.time.OffsetTime, java.time.OffsetTime]("TIME WITH TIME ZONE",
+    java.time.OffsetTime.of(1, 2, 3, 3, ZoneOffset.UTC),
+    _.withNano(0))
+  testInOutWithCustomMatch[java.time.OffsetDateTime, java.time.OffsetDateTime]("TIMESTAMP WITH TIME ZONE",
+    java.time.OffsetDateTime.of(1, 2, 3, 4, 5, 6, 7, ZoneOffset.UTC),
+    _.withNano(0))
+  testInOutWithCustomMatch[java.time.ZonedDateTime, java.time.ZonedDateTime]("TIMESTAMP WITH TIME ZONE",
+    java.time.ZonedDateTime.of(1, 2, 3, 4, 5, 6, 0, ZoneId.systemDefault()),
+    _.withFixedOffsetZone())
+  testInOut[List[Byte]]("BINARY", BigInt("DEADBEEF", 16).toByteArray.toList)
   skip("OTHER")
   testInOut[String]("VARCHAR", "abc")
   testInOut[String]("CHAR(3)", "abc")
   skip("BLOB")
   skip("CLOB")
   testInOut[UUID]("UUID", UUID.randomUUID)
-  testInOut[List[Int]]("ARRAY", List(1,2,3))
+  testInOut[List[Int]]("ARRAY", List(1, 2, 3))
   testInOut[List[String]]("ARRAY", List("foo", "bar"))
   skip("GEOMETRY")
 
