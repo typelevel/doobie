3a22b98bd9b3f411bcc066313c4f12835cf9ff36
Caoilte O'Connor <caoilte@gmail.com>
2020-08-30 22:21:23 +0100

    try and make it clear it's sql not scala

--- modules/core/src/main/scala/doobie/util/fragments.scala
+++ modules/core/src/main/scala/doobie/util/fragments.scala
@@ -16,7 +16,7 @@ object fragments {
   def in[F[_]: Reducible, A: util.Put](f: Fragment, fs: F[A]): Fragment =
     fs.toList.map(a => fr0"$a").foldSmash1(f ++ fr0"IN (", fr",", fr")")
 
-  /** Returns `f IN ((fs00: A, fs01: B), (fs10: A, fs11: B), ...)`. */
+  /** Returns `f IN ((fs0-A, fs0-B), (fs1-A, fs1-B), ...)`. */
   def in[F[_]: Reducible, A: util.Put, B: util.Put](f: Fragment, fs: F[(A,B)]): Fragment =
     fs.toList.map { case (a,b) => fr0"($a,$b)" }.foldSmash1(f ++ fr0"IN (", fr",", fr")")
 
