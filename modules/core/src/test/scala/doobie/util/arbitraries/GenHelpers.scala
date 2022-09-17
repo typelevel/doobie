8581cd99546e6e8949ad3156bebecddfd87a341f
Sam Guymer <sam@guymer.me>
2022-09-17 12:37:44 +1000

    Increase check test time resolution
    When checking if time related columns are written and read, use the
    highest resolution supported by the underlying database.

---
+++ modules/core/src/test/scala/doobie/util/arbitraries/GenHelpers.scala
@@ -0,0 +1,26 @@
+// Copyright (c) 2013-2020 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package doobie.util.arbitraries
+
+import scala.math.Ordering.Implicits._
+
+import org.scalacheck.Gen
+
+object GenHelpers {
+
+  /**
+   * [[Gen.chooseNum]] but allows `T` to have an `Ordering` instead of `Numeric`.
+   */
+  def chooseT[T](minT: T, maxT: T, specials: T*)(implicit ord: Ordering[T], c: Gen.Choose[T]): Gen[T] = {
+    val basics = List(minT, maxT)
+    val basicsAndSpecials = for {
+      t <- specials ++ basics if t >= minT && t <= maxT
+    } yield (1, Gen.const(t))
+    val other = (basicsAndSpecials.length, c.choose(minT, maxT))
+    val allGens = basicsAndSpecials :+ other
+    Gen.frequency(allGens: _*)
+  }
+
+}
