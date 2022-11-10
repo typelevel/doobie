8f2a22cd3b8720e5489d4734cef4a290b52ff691
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-11 00:06:45 +0900

    Add checkOutput method

--- modules/specs2/src/main/scala/doobie/specs2/analysisspec.scala
+++ modules/specs2/src/main/scala/doobie/specs2/analysisspec.scala
@@ -5,6 +5,7 @@
 package doobie.specs2
 
 import cats.effect.{ Async, IO }
+import doobie.{ Update, Update0 }
 import doobie.syntax.connectionio._
 import doobie.util.query.{ Query, Query0 }
 import doobie.util.testing._
@@ -46,9 +47,19 @@ object analysisspec {
         s"Query0[${typeName[A]}]", q.pos, q.sql, q.outputAnalysis
       ))
 
-    def checkOutput[A: TypeName, B: TypeName](q: Query[A, B]) =
+    def checkOutput[A: TypeName, B: TypeName](q: Query[A, B]): Fragments =
       checkImpl(AnalysisArgs(
         s"Query[${typeName[A]}, ${typeName[B]}]", q.pos, q.sql, q.outputAnalysis
+      ))
+
+    def checkOutput[A: TypeName](u: Update[A]): Fragments =
+      checkImpl(AnalysisArgs(
+        s"Update[${typeName[A]}]", u.pos, u.sql, u.analysis
+      ))
+
+    def checkOutput(u: Update0): Fragments =
+      checkImpl(AnalysisArgs(
+        "Update0", u.pos, u.sql, u.analysis
       ))
 
     private def checkImpl(args: AnalysisArgs): Fragments =
