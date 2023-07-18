ddd05b93aae696a92559523055e4a6a99b5fbe19
Ondra Pelech <ondra.pelech@gmail.com>
2023-07-19 00:10:14 +0200

    Add module for log4cats support
    Creates a Log4CatsLogHandler class

---
+++ modules/log4cats/src/main/scala/doobie/log4cats/Log4CatsLogHandler.scala
@@ -0,0 +1,48 @@
+// Copyright (c) 2013-2020 Rob Norris and Contributors
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package doobie.log4cats
+
+import doobie.LogHandler
+import doobie.util.log._
+import org.typelevel.log4cats._
+
+class Log4CatsLogHandler[F[_]](logger: MessageLogger[F]) extends LogHandler[F]{
+  override def run(logEvent: LogEvent): F[Unit] = logEvent match {
+    case Success(s, a, l, e1, e2) =>
+      logger.info(
+        s"""Successful Statement Execution:
+           |
+           |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+           |
+           | arguments = [${a.mkString(", ")}]
+           | label     = $l
+           |   elapsed = ${e1.toMillis.toString} ms exec + ${e2.toMillis.toString} ms processing (${(e1 + e2).toMillis.toString} ms total)
+        """.stripMargin)
+
+    case ProcessingFailure(s, a, l, e1, e2, t) =>
+      logger.warn(
+        s"""Failed Resultset Processing:
+           |
+           |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+           |
+           | arguments = [${a.mkString(", ")}]
+           | label     = $l
+           |   elapsed = ${e1.toMillis.toString} ms exec + ${e2.toMillis.toString} ms processing (failed) (${(e1 + e2).toMillis.toString} ms total)
+           |   failure = ${t.getMessage}
+        """.stripMargin)
+
+    case ExecFailure(s, a, l, e1, t) =>
+      logger.error(
+        s"""Failed Statement Execution:
+           |
+           |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+           |
+           | arguments = [${a.mkString(", ")}]
+           | label     = $l
+           |   elapsed = ${e1.toMillis.toString} ms exec (failed)
+           |   failure = ${t.getMessage}
+        """.stripMargin)
+  }
+}
