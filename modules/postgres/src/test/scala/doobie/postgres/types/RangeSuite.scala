7501e0a69f8c76e178207a7a53171d2323a1a4e4
Ihor Zadyra <zadyra.i@gmail.com>
2024-03-20 09:36:26 +0200

    Extend RangeSuite

--- modules/postgres/src/test/scala/doobie/postgres/types/RangeSuite.scala
+++ modules/postgres/src/test/scala/doobie/postgres/types/RangeSuite.scala
@@ -15,7 +15,6 @@ class RangeSuite extends munit.FunSuite {
 
   // Decoding
   test("decode should return correct int4range") {
-    assertEquals(decode[Int]("empty"), Right(Range.empty[Int]))
     assertEquals(decode[Int]("[10,100)"), Right(Range(10, 100, `[_,_)`)))
     assertEquals(decode[Int]("(10,100]"), Right(Range(10, 100, `(_,_]`)))
     assertEquals(decode[Int]("(10,100)"), Right(Range(10, 100, `(_,_)`)))
@@ -23,7 +22,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("decode should return correct int8range") {
-    assertEquals(decode[Long]("empty"), Right(Range.empty[Long]))
     assertEquals(decode[Long]("[10,100)"), Right(Range(10L, 100L, `[_,_)`)))
     assertEquals(decode[Long]("(10,100]"), Right(Range(10L, 100L, `(_,_]`)))
     assertEquals(decode[Long]("(10,100)"), Right(Range(10L, 100L, `(_,_)`)))
@@ -31,7 +29,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("decode should return correct numrange") {
-    assertEquals(decode[Float]("empty"), Right(Range.empty[Float]))
     assertEquals(decode[Float]("[3.14,33.14)"), Right(floatRange(`[_,_)`)))
     assertEquals(decode[Float]("(3.14,33.14]"), Right(floatRange(`(_,_]`)))
     assertEquals(decode[Float]("(3.14,33.14)"), Right(floatRange(`(_,_)`)))
@@ -51,7 +48,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("decode should return correct daterange") {
-    assertEquals(decode[LocalDate]("empty"), Right(Range.empty[LocalDate]))
     assertEquals(decode[LocalDate]("[-infinity,infinity]"), Right(Range(LocalDate.MIN, LocalDate.MAX, `[_,_]`)))
     assertEquals(decode[LocalDate]("[2024-01-01,2024-02-02)"), Right(localDateRange(`[_,_)`)))
     assertEquals(decode[LocalDate]("(2024-01-01,2024-02-02]"), Right(localDateRange(`(_,_]`)))
@@ -60,7 +56,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("decode should return correct tsrange") {
-    assertEquals(decode[LocalDateTime]("empty"), Right(Range.empty[LocalDateTime]))
     assertEquals(decode[LocalDateTime]("[-infinity,infinity]"), Right(Range(LocalDateTime.MIN, LocalDateTime.MAX, `[_,_]`)))
     assertEquals(decode[LocalDateTime]("[2024-01-01 00:00:00,2024-02-02 00:00:00)"), Right(localDateTimeRange(`[_,_)`)))
     assertEquals(decode[LocalDateTime]("(2024-01-01 00:00:00,2024-02-02 00:00:00]"), Right(localDateTimeRange(`(_,_]`)))
@@ -69,7 +64,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("decode should return correct tstzrange") {
-    assertEquals(decode[OffsetDateTime]("empty"), Right(Range.empty[OffsetDateTime]))
     assertEquals(decode[OffsetDateTime]("[-infinity,infinity]"), Right(Range(OffsetDateTime.MIN, OffsetDateTime.MAX, `[_,_]`)))
     assertEquals(decode[OffsetDateTime]("[2024-01-01 00:00:00+00,2024-02-02 00:00:00+00)"), Right(offsetDateTimeRange(`[_,_)`)))
     assertEquals(decode[OffsetDateTime]("(2024-01-01 00:00:00+00,2024-02-02 00:00:00+00]"), Right(offsetDateTimeRange(`(_,_]`)))
@@ -88,9 +82,23 @@ class RangeSuite extends munit.FunSuite {
     assertEquals(decode[OffsetDateTime]("[+++,+++]"), Left(InvalidValue[String, Range[OffsetDateTime]](value = "[+++,+++]", reason = "Text '+++' could not be parsed at index 1")))
   }
 
+  test("decode for partially filled should return correct range") {
+    assertEquals(decode[Int]("empty"), Right(Range.empty[Int]))
+    assertEquals(decode[Long]("empty"), Right(Range.empty[Long]))
+    assertEquals(decode[Float]("empty"), Right(Range.empty[Float]))
+    assertEquals(decode[LocalDate]("empty"), Right(Range.empty[LocalDate]))
+    assertEquals(decode[LocalDateTime]("empty"), Right(Range.empty[LocalDateTime]))
+    assertEquals(decode[OffsetDateTime]("empty"), Right(Range.empty[OffsetDateTime]))
+
+    assertEquals(decode[Double]("[,33.14)"), Right(Range[Double](None, Some(33.14D), `[_,_)`)))
+    assertEquals(decode[Double]("[3.14,)"), Right(Range[Double](Some(3.14D), None, `[_,_)`)))
+
+    assertEquals(decode[LocalDate]("[2024-01-01,infinity]"), Right(Range(dateLowerBound.toLocalDate, LocalDate.MAX, `[_,_]`)))
+    assertEquals(decode[LocalDate]("[-infinity,2024-02-02]"), Right(Range(LocalDate.MIN, dateUpperBound.toLocalDate, `[_,_]`)))
+  }
+
   // Encoding
   test("encode should return correct int4range string value") {
-    assertEquals(encode(Range.empty[Int]), "empty")
     assertEquals(encode(Range(10, 100, `[_,_)`)), "[10,100)")
     assertEquals(encode(Range(10, 100, `(_,_]`)), "(10,100]")
     assertEquals(encode(Range(10, 100, `(_,_)`)), "(10,100)")
@@ -98,7 +106,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("encode should return correct int8range string value") {
-    assertEquals(encode(Range.empty[Long]), "empty")
     assertEquals(encode(Range(10L, 100L, `[_,_)`)), "[10,100)")
     assertEquals(encode(Range(10L, 100L, `(_,_]`)), "(10,100]")
     assertEquals(encode(Range(10L, 100L, `(_,_)`)), "(10,100)")
@@ -106,21 +113,18 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("encode should return correct numrange string value") {
-    assertEquals(encode(Range.empty[Float]), "empty")
     assertEquals(encode(floatRange(`[_,_)`)), "[3.14,33.14)")
     assertEquals(encode(floatRange(`(_,_]`)), "(3.14,33.14]")
     assertEquals(encode(floatRange(`(_,_)`)), "(3.14,33.14)")
     assertEquals(encode(floatRange(`[_,_]`)), "[3.14,33.14]")
     assertEquals(encode(floatRange(`[_,_]`)), "[3.14,33.14]")
 
-    assertEquals(encode(Range.empty[Double]), "empty")
     assertEquals(encode(doubleRange(`[_,_)`)), "[3.14,33.14)")
     assertEquals(encode(doubleRange(`(_,_]`)), "(3.14,33.14]")
     assertEquals(encode(doubleRange(`(_,_)`)), "(3.14,33.14)")
     assertEquals(encode(doubleRange(`[_,_]`)), "[3.14,33.14]")
     assertEquals(encode(doubleRange(`[_,_]`)), "[3.14,33.14]")
 
-    assertEquals(encode(Range.empty[BigDecimal]), "empty")
     assertEquals(encode(bigDecimalRange(`[_,_)`)), "[3.14,33.14)")
     assertEquals(encode(bigDecimalRange(`(_,_]`)), "(3.14,33.14]")
     assertEquals(encode(bigDecimalRange(`(_,_)`)), "(3.14,33.14)")
@@ -129,7 +133,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("encode should return correct daterange string value") {
-    assertEquals(encode(Range.empty[LocalDate]), "empty")
     assertEquals(encode(Range[LocalDate](LocalDate.MIN, LocalDate.MAX, `[_,_]`)), "[-infinity,infinity]")
     assertEquals(encode(localDateRange(`[_,_)`)), "[2024-01-01,2024-02-02)")
     assertEquals(encode(localDateRange(`(_,_]`)), "(2024-01-01,2024-02-02]")
@@ -138,7 +141,6 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("encode should return correct tsrange string value") {
-    assertEquals(encode(Range.empty[LocalDateTime]), "empty")
     assertEquals(encode(Range(LocalDateTime.MIN, LocalDateTime.MAX, `[_,_]`)), "[-infinity,infinity]")
     assertEquals(encode(localDateTimeRange(`[_,_)`)), "[2024-01-01 00:00:00,2024-02-02 00:00:00)")
     assertEquals(encode(localDateTimeRange(`(_,_]`)), "(2024-01-01 00:00:00,2024-02-02 00:00:00]")
@@ -147,12 +149,28 @@ class RangeSuite extends munit.FunSuite {
   }
 
   test("encode should return correct tstzrange string value") {
-    assertEquals(encode(Range.empty[OffsetDateTime]), "empty")
     assertEquals(encode(Range(OffsetDateTime.MIN, OffsetDateTime.MAX, `[_,_]`)), "[-infinity,infinity]")
     assertEquals(encode(offsetDateTimeRange(`[_,_)`)), "[2024-01-01 00:00:00+00,2024-02-02 00:00:00+00)")
     assertEquals(encode(offsetDateTimeRange(`(_,_]`)), "(2024-01-01 00:00:00+00,2024-02-02 00:00:00+00]")
     assertEquals(encode(offsetDateTimeRange(`(_,_)`)), "(2024-01-01 00:00:00+00,2024-02-02 00:00:00+00)")
     assertEquals(encode(offsetDateTimeRange(`[_,_]`)), "[2024-01-01 00:00:00+00,2024-02-02 00:00:00+00]")
+  }
+
+  test("encode for partially filled should return correct string value") {
+    assertEquals(encode(Range.empty[Int]), "empty")
+    assertEquals(encode(Range.empty[Long]), "empty")
+    assertEquals(encode(Range.empty[Float]), "empty")
+    assertEquals(encode(Range.empty[Double]), "empty")
+    assertEquals(encode(Range.empty[BigDecimal]), "empty")
+    assertEquals(encode(Range.empty[LocalDate]), "empty")
+    assertEquals(encode(Range.empty[LocalDateTime]), "empty")
+    assertEquals(encode(Range.empty[OffsetDateTime]), "empty")
+
+    assertEquals(encode(Range[Double](None, Some(33.14D), `[_,_)`)), "[,33.14)")
+    assertEquals(encode(Range[Double](Some(3.14D), None, `[_,_)`)), "[3.14,)")
+
+    assertEquals(encode(Range(dateLowerBound.toLocalDate, LocalDate.MAX, `[_,_]`)), "[2024-01-01,infinity]")
+    assertEquals(encode(Range(LocalDate.MIN, dateUpperBound.toLocalDate, `[_,_]`)), "[-infinity,2024-02-02]")
   }
 
   private val dateLowerBound = OffsetDateTime.of(LocalDateTime.of(2024, 1, 1, 0, 0), ZoneOffset.UTC)
