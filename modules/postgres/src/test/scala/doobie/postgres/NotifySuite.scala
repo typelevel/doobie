0b6d58df50c8128c2bef080b664ef6aed17a7d32
Praver Bajaj <praverbajaj1609@gmail.com>
2025-03-10 07:59:13 +0530

    Migrated NotifySuite tests

--- modules/postgres/src/test/scala/doobie/postgres/NotifySuite.scala
+++ modules/postgres/src/test/scala/doobie/postgres/NotifySuite.scala
@@ -4,46 +4,45 @@
 
 package doobie.postgres
 
-import cats.effect.{IO, Sync}
+import cats.effect.{IO}
 import cats.syntax.all.*
 import doobie.*
 import doobie.implicits.*
 import org.postgresql.PGNotification
 
-class NotifySuite extends munit.FunSuite {
+class NotifySuite extends munit.CatsEffectSuite {
   import FC.{commit, delay}
-  import cats.effect.unsafe.implicits.global
   import PostgresTestTransactor.xa
 
   // Listen on the given channel, notify on another connection
   def listen[A](channel: String, notify: ConnectionIO[A]): IO[List[PGNotification]] =
-    (PHC.pgListen(channel) *> commit *>
-      delay { Thread.sleep(50) } *>
-      Sync[ConnectionIO].delay(notify.transact(xa).unsafeRunSync()) *>
-      delay { Thread.sleep(50) } *>
-      PHC.pgGetNotifications).transact(xa)
+  (for {
+    _ <- PHC.pgListen(channel)
+    _ <- commit
+    _ <- delay { Thread.sleep(50) }
+    _ <- notify
+    _ <- commit
+    _ <- delay { Thread.sleep(50) }
+    notifications <- PHC.pgGetNotifications
+  } yield notifications).transact(xa)
 
   test("LISTEN/NOTIFY should allow cross-connection notification") {
     val channel = "cha" + System.nanoTime.toString
     val notify = PHC.pgNotify(channel)
-    val test = listen(channel, notify).map(_.length)
-    assertEquals(test.unsafeRunSync(), 1)
+    listen(channel, notify).map(_.length).assertEquals(1)  
   }
 
   test("LISTEN/NOTIFY should allow cross-connection notification with parameter") {
     val channel = "chb" + System.nanoTime.toString
     val messages = List("foo", "bar", "baz", "qux")
     val notify = messages.traverse(PHC.pgNotify(channel, _))
-    val test = listen(channel, notify).map(_.map(_.getParameter))
-    assertEquals(test.unsafeRunSync(), messages)
+    listen(channel, notify).map(_.map(_.getParameter)).assertEquals(messages) 
   }
 
   test("LISTEN/NOTIFY should collapse identical notifications") {
     val channel = "chc" + System.nanoTime.toString
     val messages = List("foo", "bar", "bar", "baz", "qux", "foo")
     val notify = messages.traverse(PHC.pgNotify(channel, _))
-    val test = listen(channel, notify).map(_.map(_.getParameter))
-    assertEquals(test.unsafeRunSync(), messages.distinct)
+    listen(channel, notify).map(_.map(_.getParameter)).assertEquals(messages.distinct) 
   }
-
 }
