ab8a2e85fe2bd30cb0c6c64fd7b7895f3cc80351
Praver Bajaj <praverbajaj1609@gmail.com>
2025-03-11 04:17:26 +0530

    Solved formatting issues

--- modules/postgres/src/test/scala/doobie/postgres/NotifySuite.scala
+++ modules/postgres/src/test/scala/doobie/postgres/NotifySuite.scala
@@ -15,24 +15,24 @@ class NotifySuite extends munit.CatsEffectSuite {
   import PostgresTestTransactor.xa
 
   // Listen on the given channel, notify on another connection
-def listen[A](channel: String, notify: ConnectionIO[A]): IO[List[PGNotification]] = 
-  WeakAsync.liftIO[ConnectionIO].use { liftIO =>
-    (for {
-      _ <- PHC.pgListen(channel)
-      _ <- commit
-      _ <- delay { Thread.sleep(50) }
-      _ <- liftIO.liftIO(notify.transact(xa))
-      _ <- commit
-      _ <- delay { Thread.sleep(50) }
-      notifications <- PHC.pgGetNotifications
-    } yield notifications).transact(xa)
-  }
+  def listen[A](channel: String, notify: ConnectionIO[A]): IO[List[PGNotification]] =
+    WeakAsync.liftIO[ConnectionIO].use { liftIO =>
+      (for {
+        _ <- PHC.pgListen(channel)
+        _ <- commit
+        _ <- delay { Thread.sleep(50) }
+        _ <- liftIO.liftIO(notify.transact(xa))
+        _ <- commit
+        _ <- delay { Thread.sleep(50) }
+        notifications <- PHC.pgGetNotifications
+      } yield notifications).transact(xa)
+    }
 
   test("LISTEN/NOTIFY should allow cross-connection notification") {
     val channel = "cha" + System.nanoTime.toString
     val notify = PHC.pgNotify(channel)
     val test = listen(channel, notify).map(_.length)
-    test.assertEquals(1)  
+    test.assertEquals(1)
   }
 
   test("LISTEN/NOTIFY should allow cross-connection notification with parameter") {
@@ -40,7 +40,7 @@ def listen[A](channel: String, notify: ConnectionIO[A]): IO[List[PGNotification]
     val messages = List("foo", "bar", "baz", "qux")
     val notify = messages.traverse(PHC.pgNotify(channel, _))
     val test = listen(channel, notify).map(_.map(_.getParameter))
-    test.assertEquals(messages) 
+    test.assertEquals(messages)
   }
 
   test("LISTEN/NOTIFY should collapse identical notifications") {
