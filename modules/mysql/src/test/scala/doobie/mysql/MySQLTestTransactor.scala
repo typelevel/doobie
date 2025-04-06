64c388e9f488c7d813bca7522b368a176ffa87fb
danicheg <esik.danny@gmail.com>
2025-04-06 13:59:17 +0300

    Fix dead link to the 'Preserving Time Instants' article on the Oracle docs

--- modules/mysql/src/test/scala/doobie/mysql/MySQLTestTransactor.scala
+++ modules/mysql/src/test/scala/doobie/mysql/MySQLTestTransactor.scala
@@ -11,7 +11,7 @@ object MySQLTestTransactor {
 
   val xa = Transactor.fromDriverManager[IO](
     "com.mysql.cj.jdbc.Driver",
-    // args from solution 2a https://docs.oracle.com/cd/E17952_01/connector-j-8.0-en/connector-j-time-instants.html
+    // args from solution 2a https://docs.oracle.com/cd/E17952_01/connector-j-en/connector-j-time-instants.html
     "jdbc:mysql://localhost:3306/world?preserveInstants=true&connectionTimeZone=SERVER",
     "root",
     "password",
